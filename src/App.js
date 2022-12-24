a067504bbe705450034f2dfdfe49935ca13cd9e0
Muskan Valmikee <muskanvalmikee@gmail.com>
2022-12-24 16:26:07 +0530

    directory app in react.

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Details from './components/Body';
+
+function App() {
+  return (
+    <Details/>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import "./index.css"
+function Header() {
+    return (
+        <div className="header">
+            <h3>Muskan's Directory App</h3>
+        </div>
+    )
+}
+
+export default Header
